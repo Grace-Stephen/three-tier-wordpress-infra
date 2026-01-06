ce8531eeb796b83d5eb3e2f7bdf0c37020e02e56
Grace-Stephen <gracestephen24@gmail.com>
2026-01-06 17:59:28 +0100

    deleted a block in variables.tf

--- envs/staging/variables.tf
+++ envs/staging/variables.tf
@@ -93,13 +93,6 @@ variable "ec2_instance_ids" {
   type = list(string)
 }
 
-variable "alb_arn_suffix" {
-  type = string
-}
-
-variable "target_group_arn_suffix" {
-  type = string
-}
 
 variable "rds_instance_identifier" {
   type = string
