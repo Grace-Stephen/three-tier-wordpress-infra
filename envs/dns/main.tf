ea3688be581f6564aec07d5687ab11f3921f0499
Grace-Stephen <gracestephen24@gmail.com>
2026-01-19 15:54:22 +0100

    infra destroy

--- envs/dns/main.tf
+++ envs/dns/main.tf
@@ -1,50 +1,8 @@
-# data "terraform_remote_state" "staging" {
-#   backend = "s3"
-#   config = {
-#     bucket = "ogoma-wp-terraform-state"
-#     key    = "staging/terraform.tfstate"
-#     region = "us-east-1"
-#   }
-# }
-
-# data "terraform_remote_state" "prod" {
-#   backend = "s3"
-#   config = {
-#     bucket = "ogoma-wp-terraform-state"
-#     key    = "production/terraform.tfstate"
-#     region = "us-east-1"
-#   }
-# }
 
 ###ROUTE 53######
  resource "aws_route53_zone" "root" {
   name = "ogomawoman.store"
 }
-
-# resource "aws_route53_record" "prod" {
-#   zone_id = aws_route53_zone.root.zone_id
-#   name    = "ogomawoman.store"
-#   type    = "A"
-
-#   alias {
-#     name                   = data.terraform_remote_state.prod.outputs.alb_dns_name
-#     zone_id                = data.terraform_remote_state.prod.outputs.alb_zone_id
-#     evaluate_target_health = true
-#   }
-# }
-
-# resource "aws_route53_record" "staging" {
-#   zone_id = aws_route53_zone.root.zone_id
-#   name    = "staging.ogomawoman.store"
-#   type    = "A"
-
-#   alias {
-#     name                   = data.terraform_remote_state.staging.outputs.alb_dns_name
-#     zone_id                = data.terraform_remote_state.staging.outputs.alb_zone_id
-#     evaluate_target_health = true
-#   }
-# }
-
 
 #### ACM CERT #####
 resource "aws_acm_certificate" "certs" {
@@ -63,9 +21,6 @@ resource "aws_route53_record" "validation" {
   records = [tolist(each.value.domain_validation_options)[0].resource_record_value]
   ttl     = 60
 }
-
-
-
 
 ## cert validation
 resource "aws_acm_certificate_validation" "certs" {
